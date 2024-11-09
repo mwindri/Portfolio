f8a26fb753df1206035f5df60f3f6465bd91406f
mwindri <149879925+mwindri@users.noreply.github.com>
2024-11-09 21:26:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/wins2.png" type="image/png">
+	<title>WINS - HOME</title>
+</head>
+
+
+
+<?php include("component/navbar.php"); ?>	
+
+	<li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>
+
+	<!--================ Start Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-7">
+						<div class="banner_content">
+							<h1 class="text-uppercase"><br>Muhammad Windri</h1>
+							<h5 class="text-uppercase"> Developer IT Support </h5>
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#"><span>HERE</span></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-5">
+						<div class="home_right_img">
+							<img class="" src="img/banner/home-right.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Home Banner Area =================-->
+
+	<!--================ Start About Us Area =================-->
+	<section class="about_area">
+		<div class="container">
+			<div class="row justify-content-start align-items-center">
+				<div class="col-lg-5">
+					<div class="about_img">
+						<img class="" src="img/about-us.png" alt="">
+					</div>
+				</div>
+
+				<div class="offset-lg-1 col-lg-5">
+					<div class="main_title text-left">
+						<h2>let's <br>
+							Introduce about <br>
+							myself</h2>
+						<p>
+							I am an experienced IT Support Developer focusing on application development and technical support.
+						</p>
+						<p>
+							With more than 10 years of experience in the IT field, I have helped many companies and individuals         
+							to overcome technical problems and ensure that their systems run smoothly and efficiently.
+							
+						</p>
+						<a class="primary_btn" href="about.php"><span>ABOUT ME</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End About Us Area =================-->
+
+	<?php include("component/footer.php"); ?>
+	
